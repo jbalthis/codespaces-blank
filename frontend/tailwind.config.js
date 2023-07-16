659e6d23a20bfecb19ec981f3293158020bdc19a
Jason Balthis <jbalthis1981@yahoo.com>
2023-07-16 06:00:09 +0000

    build: tailwind config

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -1,18 +1,32 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = {
+export default {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './components/**/*.{js,ts,jsx,tsx,mdx}',
-    './app/**/*.{js,ts,jsx,tsx,mdx}',
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
   theme: {
     extend: {
-      backgroundImage: {
-        'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
-        'gradient-conic':
-          'conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))',
+      screens: {
+        sm: "480px",
+        md: "768px",
+        lg: "976px",
+        xl: "1440px",
+      },
+      colors: {
+        blue: "#7C96AB",
+        purple: "#B2A4FF",
+        pink: "#E8A0BF",
+        orange: "#FFD4B2",
+        green: "#CEEDC7",
+        yellow: "#FFF6BD",
+        grayDark: "#555555",
+        gray: "#CDC9C3",
+        grayLight: "#FBF7F0",
+      },
+      fontFamily: {
+        sans: ["Abel", "sans-serif"],
       },
     },
   },
   plugins: [],
-}
+};
